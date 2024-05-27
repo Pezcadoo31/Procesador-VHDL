7b67903f2158d22e30986a11e4bb045571ae79b7
Pezcadoo31 <A01750922@tec.mx>
2024-05-26 23:08:01 -0600

    Inicio de Serie Fibonacci

--- MEM_PROGRAMA/MEM_PROGRAMA.vhd
+++ MEM_PROGRAMA/MEM_PROGRAMA.vhd
@@ -47,16 +47,34 @@ Constant JNC			: std_logic_vector (7 downto 0)	:= x"28"; -- SALTO A DIRECCION SI
 
 	type INSTMEM is array (0 to 127) of std_logic_vector (7 downto 0); 
 
-Signal ROM: INSTMEM :=(	0			=> LOAD_DIR_A, -- LOOP INFINITO, LEE EL PUERTO E0 Y ESCRIBE EL DATO EN 
-									1			=> x"E0",
-									2			=> LOAD_INM_B,
-									3			=> "00000011",
-									4			=> ADD_AB,
-									5			=> STORE_A,
-									6			=> X"FF",
-									7			=> JMP,
-									8			=> x"00",
-									others	=> x"00");	-- Las demás posiciones se inicializan a 0.
+--Signal ROM: INSTMEM :=(	0			=> LOAD_DIR_A, -- LOOP INFINITO, LEE EL PUERTO E0 Y ESCRIBE EL DATO EN 
+--									1			=> x"E0",
+--									2			=> LOAD_INM_B,
+--									3			=> "00000011",
+--									4			=> ADD_AB,
+--									5			=> STORE_A,
+--									6			=> X"FF",
+--									7			=> JMP,
+--									8			=> x"00",
+--									others	=> x"00");	-- Las demás posiciones se inicializan a 0.
+
+signal ROM: INSTMEM := (
+    0     => LOAD_DIR_A,  -- Inicializar A con 0
+    1     => X"00",        -- Dirección de inicio de la serie Fibonacci
+    2     => LOAD_DIR_B,   -- Inicializar B con 1
+    3     => X"01",        -- Valor 1 para comenzar la serie Fibonacci
+    4     => ADD_AB,       -- Sumar A y B
+    5     => STORE_A,      -- Almacenar el resultado en A
+    6     => X"00",        -- Dirección de inicio de la serie Fibonacci
+    7     => LOAD_INM_B,   -- Cargar el valor 1 en B para la siguiente iteración
+    8     => X"01",        -- Valor 1 para cargar en B
+    9     => JMP,          -- Saltar de nuevo al inicio de la serie Fibonacci
+    10    => X"FF",        -- Dirección de memoria para mostrar resultados
+    11    => LOAD_OUT_A,   -- Cargar el resultado desde la dirección de memoria
+    12    => X"FF",        -- Dirección de memoria X"FF"
+    others => X"00"
+);
+
 Begin
 -- Proceso para la lectura de la memoria de programa
 	Process (CLK)
